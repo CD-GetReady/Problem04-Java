4fbd55c328fc6556beac0c75a505ea1c184d11cf
cyanthecomputerscientist <cyanthecomputerscientist@gmail.com>
2020-09-30 18:56:21 -0400

    The test class was default and not public

--- src/test/java/cd/get/ready/algorithms/DogWalkerTest.java
+++ src/test/java/cd/get/ready/algorithms/DogWalkerTest.java
@@ -3,14 +3,14 @@ package cd.get.ready.algorithms;
 import org.junit.Assert;
 import org.junit.Test;
 
-class DogWalkerTest {
+public class DogWalkerTest {
 
     @Test
     public void testOne(){
         DogWalker dw = new DogWalker();
         int[] dogs = {1, 1, 3, 1, 2, 1, 3, 3, 3, 3};
-        int expected = dw.numberOfWalksCalculator(dogs);
-        int actual = 6;
+        int actual = dw.numberOfWalksCalculator(dogs);
+        int expected = 6;
         Assert.assertEquals(expected, actual);
     }
 
@@ -18,8 +18,8 @@ class DogWalkerTest {
     public void testTwo(){
         DogWalker dw = new DogWalker();
         int[] dogs = {10, 20, 10, 10, 30, 50, 10, 20, 30};
-        int expected = dw.numberOfWalksCalculator(dogs);
-        int actual = 5;
+        int actual = dw.numberOfWalksCalculator(dogs);
+        int expected = 5;
         Assert.assertEquals(expected, actual);
     }
 
