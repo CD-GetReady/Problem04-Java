93fd36cb134cbbd9ee1071b93c5a2b5abbc692b2
cyanthecomputerscientist <cyanthecomputerscientist@gmail.com>
2020-09-30 18:22:22 -0400

    Working on fixing unit test

--- src/main/java/cd/get/ready/algorithms/DogWalker.java
+++ src/main/java/cd/get/ready/algorithms/DogWalker.java
@@ -1,8 +1,17 @@
 package cd.get.ready.algorithms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DogWalker {
     public int numberOfWalksCalculator(int[] dogSizes){
-        int numberOfWalks = -1;
+        
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < dogSizes.length; i++) 
+        {
+            list.add(dogSizes[i]);
+        }
+        int numberOfWalks = list.size();
         return numberOfWalks;
     }
 }
